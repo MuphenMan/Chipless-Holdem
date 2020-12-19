16799cb95f3bc8149e539d35b1c1abaf1191f7e7
MuphenMan <51278624+MuphenMan@users.noreply.github.com>
2020-12-18 22:05:40 -0800

    Update Game.h

--- Game.h
+++ Game.h
@@ -3,13 +3,13 @@
 
 class Game {
 	public:
-		Game	g(Player p[], int numPlayers);
-		void	setHiLow();
-		double	getHi();
-		double	getLow();
-		void	displayTotals(Player p[], int numPlayers);
-		double	getPot();
-		double	setPot(double pot);
+		Game	g(Player p[], int numPlayers);				//will have to change Player array into a Player address for linked list
+		void	setHiLow();						//set High and low of game
+		double	getHi();						//accessor of games high 
+		double	getLow();						//accessor of games low
+		void	displayTotals(Player p[], int numPlayers);		//simple function to display player totals
+		double	getPot();						//accessor function for the table pot
+		double	setPot(double pot);					//mutator for table pot
 
 	private:
 		double gamePot;
